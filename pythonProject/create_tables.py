12b48234e677ec0ba548047fd2461d869f41cfc9
YannickBihege <yannickbtheone@hotmail.com>
2024-03-03 18:29:52 +0100

    [001] Add docstrings comments

--- pythonProject/create_tables.py
+++ pythonProject/create_tables.py
@@ -4,6 +4,16 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drops database tables based on SQL queries defined in the drop_table_queries.
+
+    Args:
+        cur: a database cursor reference, used to perform database operations
+        conn: a database connection reference
+
+    Raises:
+        Exception: if unable to execute any SQL query
+    """
     for query in drop_table_queries:
         try:
             cur.execute(query)
@@ -11,7 +21,18 @@ def drop_tables(cur, conn):
         except Exception as e:
             print(f"Failed to execute query: {query}", e)
 
+
 def create_tables(cur, conn):
+    """
+    Creates database tables based on SQL queries defined in the create_table_queries.
+
+    Args:
+        cur: a database cursor reference, used to perform database operations
+        conn: a database connection reference
+
+    Raises:
+        Exception: if unable to execute any SQL query
+    """
     for query in create_table_queries:
         try:
             cur.execute(query)
@@ -21,6 +42,13 @@ def create_tables(cur, conn):
 
 
 def main():
+    """
+    Connects to a PostgreSQL database, drops existing tables, and then creates new ones.
+    Reads database config from a config file 'dwh.cfg'
+
+    Raises:
+        Exception: if unable to connect to the database, or other general exception
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
     try:
@@ -40,7 +68,6 @@ def main():
         cur.close()
         conn.close()
         print("Database connection closed")
-
 
 
 if __name__ == "__main__":
--- pythonProject/etl.py
+++ pythonProject/etl.py
@@ -4,6 +4,16 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Loads data into staging tables based on SQL queries defined in the copy_table_queries.
+
+    Args:
+        cur: a database cursor reference, used to perform database operations
+        conn: a database connection reference
+
+    Raises:
+        Exception: if unable to execute SQL query
+    """
     for query in copy_table_queries:
         try:
             cur.execute(query)
@@ -13,6 +23,16 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
+    """
+    Inserts data into tables defined in the database based on the SQL queries defined in the insert_table_queries.
+
+    Args:
+        cur: a database cursor reference, used to perform database operations.
+        conn: a database connection reference.
+
+    Raises:
+        Exception: if unable to execute SQL query.
+    """
     for query in insert_table_queries:
         try:
             cur.execute(query)
@@ -22,6 +42,10 @@ def insert_tables(cur, conn):
 
 
 def main():
+    """
+    Connects to a PostgreSQL database, loads data into staging tables, and then inserts data into final tables.
+    Reads database config details from a config file 'dwh.cfg'
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
